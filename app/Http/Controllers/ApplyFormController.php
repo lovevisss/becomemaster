0136fed65c011f5d11da0ead8e84b691da8336d3
zsy <530822987@qq.com>
2024-10-26 09:15:57 +0000

    add apply form and department

---
+++ app/Http/Controllers/ApplyFormController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ApplyForm;
+use Illuminate\Http\Request;
+
+class ApplyFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ApplyForm $applyForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ApplyForm $applyForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ApplyForm $applyForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ApplyForm $applyForm)
+    {
+        //
+    }
+}
--- database/migrations/2023_09_26_054557_create_projects_table.php
+++ database/migrations/2023_09_26_054557_create_projects_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedInteger('project_type_id');
+            $table->unsignedInteger('department_id');
             $table->longText('description')->nullable();
              $table->integer('company_id')->unsigned()->nullable();
             // $table->integer('user_id')->unsigned()->nullable();
